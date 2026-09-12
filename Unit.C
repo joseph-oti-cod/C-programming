6fd60b3b2847e1ba4c6555d89fe38070667b23cb
joseph-oti-cod <oduruotieno@students.zetech.ac.ke>
2026-09-12 13:20:07 +0300

    Add files via upload

---
+++ Unit.C
@@ -0,0 +1,24 @@
+//Single line comment
+//A Program to display the Units offered this semester
+
+/*
+Author: Joseph Otieno Oduru
+Reg Number: BCS-05-0161/2024
+Description: printing the units offerd this semester
+Date: 12TH SEP 2026
+Version: 1.1
+*/
+
+#include<stdio.h> // printf(), scanf()
+int main(){
+	printf ("Units offered this semester\n");
+	printf ("1.Computational Logic\n");
+	printf ("2.Communication Skills\n");
+	printf ("3.Introduction to computers and internet\n");
+	printf ("4.Health And Wellness Management\n");
+	printf ("5.Structured Programming And Algorithms\n");
+	printf ("6.Basic Mathematics\n");
+	printf ("7.Mathematical Physics\n");
+	
+	return 0;
+}
